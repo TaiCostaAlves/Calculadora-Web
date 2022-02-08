ca0f73a69d9b40d139121a79800e068980b11266
tainaracosta92@hotmail.com <tainaracosta92@hotmail.com>
2022-02-07 21:29:39 -0300

    Desenvolvendo o script do projeto

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const resultado = document.querySelector(" .result ");
-const confirmar = document.querySelector( ".igual ");
+const confirmar = document.querySelector(".igual ");
+
+function insert(valor) {
+    resultado.innerHTML += valor;
+}
+
+function clean() {
+    resultado.innerHTML = ' ';
+}
+
+function backspace(){
+    if(resultado.textContent){
+        let result = document.getElementById('resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length - 1);
+    }
+}
